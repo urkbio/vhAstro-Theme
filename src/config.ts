c44b5556d99f31fb8f91d457bd8242635a40dcfa
urkbio <uniquezyg@outlook.com>
2025-04-10 12:38:08 +0800

    change favicon & remove reward

--- src/config.ts
+++ src/config.ts
@@ -108,19 +108,19 @@ export default {
   HanAnalytics: { enable: true, server: 'https://analytics.235421.xyz', siteId: 'vhAstro' },
   // Google 广告
   GoogleAds: {
-    ad_Client: '', //ca-pub-xxxxxx
+    ad_Client: 'ca-pub-4794157925903709', //ca-pub-xxxxxx
     // 侧边栏广告(不填不开启)
     asideAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-4794157925903709" data-ad-slot="5326559858" data-ad-format="auto" data-full-width-responsive="true"></ins>`,
     // 文章页广告(不填不开启)
     articleAD_Slot: `<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-4794157925903709" data-ad-slot="1321464723"></ins>`
   },
   // 文章内赞赏码
-  Reward: {
-    // 支付宝收款码
-    AliPay: '',
-    // 微信收款码
-    WeChat: ''
-  },
+  // Reward: {
+  //   // 支付宝收款码
+  //   AliPay: '',
+  //   // 微信收款码
+  //   WeChat: ''
+  // },
   // 访问网页 自动推送到搜索引擎
   SeoPush: {
     enable: false,
